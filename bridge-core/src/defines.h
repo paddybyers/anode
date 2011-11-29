00e86d11c0c47a17140ab388169be249d8816727
Paddy Byers <paddy.byers@gmail.com>
2011-11-29 20:51:35 +0000

    Add error codes

--- bridge-core/src/defines.h
+++ bridge-core/src/defines.h
@@ -3,4 +3,14 @@
 
 #define LIB_EXPORT __attribute__ ((visibility("default")))
 
+enum {
+	OK            = 0,
+	ErrorMem      = -1,
+	ErrorConfig   = -2,
+	ErrorVM       = -3,
+	ErrorIO       = -4,
+	ErrorNotfound = -5,
+	ErrorInvalid  = -6
+};
+
 #endif
