56f7bc783e48a6020c61c41fa1770157bcda2a2d
Paddy Byers <paddy.byers@gmail.com>
2012-01-05 14:35:31 +0000

    Re-generated

--- libnode/gen/org/meshpoint/anode/R.java
+++ libnode/gen/org/meshpoint/anode/R.java
@@ -11,13 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static int icon=0x7f020000;
+        public static final int icon=0x7f020000;
     }
     public static final class layout {
-        public static int main=0x7f030000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static int app_name=0x7f040001;
-        public static int hello=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
 }
