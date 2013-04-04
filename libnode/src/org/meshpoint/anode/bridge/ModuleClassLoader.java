17ad330078df6416b5520a2b13a77c58b2c861ae
Paddy Byers <paddy.byers@gmail.com>
2013-04-04 15:23:02 +0100

    Load classes from default application classloader

--- libnode/src/org/meshpoint/anode/bridge/ModuleClassLoader.java
+++ libnode/src/org/meshpoint/anode/bridge/ModuleClassLoader.java
@@ -6,37 +6,44 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import dalvik.system.DexClassLoader;
 
 public class ModuleClassLoader extends ClassLoader {
-  private static String packageName = "org.webinos.modules";
- 
-  private DexClassLoader internalClassLoader;
+	private static String packageName = "org.webinos.modules";
 
-  public ModuleClassLoader(Object envContext) {
-    if (!(envContext instanceof Context)) {
-      throw new IllegalArgumentException();
-    }
-    Context androidContext = (Context) envContext;
-    PackageManager pkgMgr = androidContext.getPackageManager();
+	private ClassLoader defaultClassLoader;
+	private DexClassLoader internalClassLoader;
 
-    if (pkgMgr != null
-        && pkgMgr.checkSignatures(androidContext.getPackageName(), packageName) >= 0) {
-      String dexPath = null;
-      try {
-        dexPath = pkgMgr.getApplicationInfo(packageName, 0).sourceDir;
-      } catch (NameNotFoundException e) {}
-      if (dexPath != null) {
-        String dexOutputDir = androidContext.getDir("dex", 0).getAbsolutePath();
-        internalClassLoader = new DexClassLoader(dexPath, dexOutputDir, null,
-            androidContext.getClassLoader());
-      }
-    }
-  }
+	public ModuleClassLoader(Object envContext) {
+		if (!(envContext instanceof Context)) {
+			throw new IllegalArgumentException();
+		}
+		Context androidContext = (Context) envContext;
+		defaultClassLoader = androidContext.getClassLoader();
+		PackageManager pkgMgr = androidContext.getPackageManager();
 
-  @Override
-  public Class<?> findClass(String className) throws ClassNotFoundException {
-    if (internalClassLoader != null) {
-      return internalClassLoader.loadClass(className);
-    } else {
-      throw new ClassNotFoundException();
-    }
-  }
+		if (pkgMgr != null
+				&& pkgMgr.checkSignatures(androidContext.getPackageName(), packageName) >= 0) {
+			String dexPath = null;
+			try {
+				dexPath = pkgMgr.getApplicationInfo(packageName, 0).sourceDir;
+			} catch (NameNotFoundException e) {}
+			if (dexPath != null) {
+				String dexOutputDir = androidContext.getDir("dex", 0).getAbsolutePath();
+				internalClassLoader = new DexClassLoader(dexPath, dexOutputDir, null,
+						androidContext.getClassLoader());
+			}
+		}
+	}
+
+	@Override
+	public Class<?> findClass(String className)  throws ClassNotFoundException {
+		Class<?> clazz;
+		try {
+			if((clazz = defaultClassLoader.loadClass(className)) != null)
+				return clazz;
+		} catch(ClassNotFoundException cnfe) {}
+
+		if (internalClassLoader != null && (clazz = internalClassLoader.loadClass(className)) != null)
+			return clazz;
+
+		throw new ClassNotFoundException(className);
+	}
 }
